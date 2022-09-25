876368bef62cb693f779332bd18c5abcabc0a607
Mariuskvaal <mariuskvaal1@gmail.com>
2022-09-25 16:32:07 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-function ShowLi(){
-    document.querySelector(".Hamburger_menu")
+const menuItems = document.querySelector(".Hamburger_menu");
+
+menuItems.style.dis  play: "none";
+
+function menutoggle () {
+    if(menuItems.style.maxHeight == "0px")
+    {
+        menuItems.style.maxHeight = "200px"
+    }
+    else
+    {
+        menuItems.style.maxHeight = "0px"
+    }
 }
+
