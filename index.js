f01f4156bdd235eb1364e24d458a819145c18f94
Mariuskvaal <mariuskvaal1@gmail.com>
2022-10-07 17:14:22 +0200

    updating front page

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// Picture swapping toggle // 
+
+
+var productImg = document.getElementById("productImg");
+
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].oncli
+
+
+
+
+
+
+
+
