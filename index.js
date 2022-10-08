90b5d95a07a1a0e935563b607f1befa32a6405d3
Mariuskvaal <mariuskvaal1@gmail.com>
2022-10-08 14:52:44 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 
-var ProductImg = document.querySelector("#ProductImg");
+let ProductImg = document.getElementById("ProductImg");
 
-var smallImg = document.querySelector(".smallImg");
+let smallImg = document.getElementsByClassName("smallImg");
+
+smallImg[0].onclick = function() {
+    ProductImg.src = smallImg[0].src;
+
+}
+
+smallImg[1].onclick = function() {
+    ProductImg.src = smallImg[1].src;
+
+}
+
+smallImg[2].onclick = function() {
+    ProductImg.src = smallImg[2].src;
+}
+
+smallImg[3].onclick = function() {
+    ProductImg.src = smallImg[3].src;
+
+}
+
+
+// <!---- CART FUNCTION -->
+
+
+
+
+
+
+
+
 
 
 
