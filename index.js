4a3301a8ddf8e60c26e6e34a3e9e88518f1ba99d
Mariuskvaal <mariuskvaal1@gmail.com>
2022-10-09 16:19:28 +0200

    updating the page

--- index.js
+++ index.js
@@ -19,11 +19,32 @@ smallImg[2].onclick = function() {
 
 smallImg[3].onclick = function() {
     ProductImg.src = smallImg[3].src;
-
 }
 
 
-// <!---- CART FUNCTION -->
+
+
+
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+function validateName(){
+    var name = document.getElementById("contact-name").value;
+
+    if(name.length == 0){
+        nameError.innerHTML = "name is required";
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = "write full name";
+        return false;
+    }
+    nameError.innerHTML = "valid";
+    return true;
+};
 
 
 
@@ -44,3 +65,98 @@ smallImg[3].onclick = function() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+// <!---- FORM FUNCTION -->
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add("error");
+    inputControl.classList.remove("success");
+
+}
+
+const setSuccess = element=> {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector("error");
+
+    errorDisplay.innerText = "";
+    inputControl.classList.add("success");
+    inputControl.classList.remove("error");
+};
+
+const validateInputs = () => {
+    const usernameValue = username.Value.trim();
+    const emailValue = email.Value.trim();
+    const passwordValue = password.Value.trim();
+    const password2Value = password2.Value.trim();
+
+    if(usernameValue === "") {
+        setError(username, "username is required");
+    } else {
+          setSuccess(username);
+    }
+
+    if(emailValue === "") {
+        setError(email, "email is required");
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === "") {
+        setError(password, "password is required");
+    } else if (passwordValue.length < 8 ) {
+        setError(password, "password must be at least 8 character.")
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === ""){
+        setError(password2, "please confirm your password");
+    } else if (password2Value !== passwordValue) {
+        setError(password2    )
+    }
+};
