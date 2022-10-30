3034b0ddfa49807ec9555c323c8854b517f63f93
Mariuskvaal <mariuskvaal1@gmail.com>
2022-10-30 15:37:47 +0100

    updating the wordpress api

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+const url = "https://bigflowerpower.online/wp-json/wc/store/products";
+const productContainer = document.querySelector(".products");
 
-    const baseUrl = "https://bigflowerpower.online/wp-json/wc/store/products";
-
-    const productContainer = document.querySelector(".products");
-
-    async function getProducts(url){
+async function getProducts(){
+    try{
         const response = await fetch(url);
-        const products = await response.json();
+        const getResults = await response.json();
+        createHTML(getResults);
     }
+    catch(error){
+        console.log(error);
+    }
+}
 
-    getProducts(baseUrl);
+getProducts();
+
+function createHTML(products){
+    products.forEach(function(product){
+        console.log(product)
+        productContainer.innerHTML += 
+            `<div class="product">
+                <h3>${product.name}</h3>
+                <img src="${product.images[0].src}" alt="${product.name}">
+                             
+            </div>`;
+
+    })
+
+
+}
